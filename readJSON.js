f9190271eb688625eab9c1f6c8b6e10776f621fc
cliao2 <sophia2014cs@gmail.com>
2019-10-13 10:15:47 -0500

    can read to integer

--- readJSON.js
+++ readJSON.js
@@ -2,14 +2,9 @@ const fs = require('fs');
 var obj = require('./NGSCOutput/g_24_2.gexf.json');
 //node
 var nodeObj = obj.gexf.graph.nodes.node
+var edgeObj = obj.gexf.graph.edges
 var i;
-for (i = 0; i < nodeOjb.length; i++) {
-  text += cars[i] + "<br>";
+for (i = 0; i < nodeObj.length; i++) {
+  //console.log(nodeObj[i])
+  console.log(nodeObj[i]['@id'])
 }
-console.log(nodeObj)
-/*for (i in nodeObj) {
-  console.log(i)
-  x = ;
-  console.log(x);
-}*/
-//var sMyString = fs.readFileSync('./NGSCOutput/g_24_2.gexf.json').toString();
